69c2eae3599a0b8bedef294e1e19ed5d8cdd5430
Kevin Schuetz <schuetzk@gmail.com>
2013-06-22 22:22:58 -0500

    added login.scala.html

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,6 +4,8 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
+
+  val loginDAO = models.slick.SlickLoginDAO
   
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
