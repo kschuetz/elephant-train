b10f7056b2d1ea274d7c8b05fb77fac2b03d093e
Kevin Schuetz <schuetzk@gmail.com>
2013-06-23 16:08:32 -0500

    dummy implementation for getAuthenticatedUser

--- app/controllers/Secured.scala
+++ app/controllers/Secured.scala
@@ -8,6 +8,12 @@ import play.api.libs.iteratee._
 
 trait Secured extends Controller {
 
+  import models._
+
+  private val dummyUserID = java.util.UUID.fromString("12345678-90ab-cdef-1234-56789abcdef1")
+
+  private def getAuthenticatedUser(request: RequestHeader) =
+    Some(AuthenticatedUser(dummyUserID))
 
   private def onUnauthorized(request: RequestHeader) = Results.Redirect(routes.Application.login)
 
