221b7576b3f2746f071fa9d8ced989ee2d3d60e4
Kevin Schuetz <schuetzk@gmail.com>
2013-06-22 22:58:02 -0500

    password check

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,6 +4,7 @@ import play.api._
 import play.api.data._
 import play.api.data.Forms._
 import play.api.mvc._
+import models.UserAuthResponse
 
 object Application extends Controller {
 
@@ -25,13 +26,36 @@ object Application extends Controller {
   }
 
   def authenticate = Action { implicit request =>
+
+    val now = new java.util.Date()
+    val loginTime = new java.sql.Timestamp(now.getTime)
+
+    def isThrottled(info: UserAuthResponse): Boolean = {
+      info.loginDisallowedUntil.map { loginTime.before(_) } getOrElse(false)
+    }
+
+    def checkPassword(passwordIn: String, password: String): Boolean = {
+      import com.github.t3hnar.bcrypt._
+      passwordIn.isBcrypted(password)
+    }
+
     loginForm.bindFromRequest.fold(
       formWithErrors => BadRequest(views.html.main("Login")(views.html.login(formWithErrors))),
       credentials => {
         val authInfo = loginDAO.userLoginByEmailAddress(credentials._1)
 
         authInfo match {
-          case Some(info) => Ok("hello " + info.emailAddress)
+          case Some(info) => {
+            if(isThrottled(info)) Ok("throttled")
+            else {
+              if(checkPassword(credentials._2, info.password)) {
+                loginDAO.updateSuccessForUser(info.userID, Some(loginTime))
+                Ok("hello " + info.emailAddress)
+              } else {
+                Ok("bad password")
+              }
+            }
+          }
           case _ => Ok("nope")
         }
       }
